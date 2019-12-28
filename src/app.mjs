665f7ff7537665bc8e9fd27fb6235a8827f6b264
Jascha <jascha@jaeh.at>
2019-12-28 18:08:53 +0100

    alphabetize menu by magic-library names

--- src/app.mjs
+++ src/app.mjs
@@ -3,5 +3,17 @@ export const state = {
   description: '@magic-libraries. tiny client side javascript @magic.',
   logotext: '@magic-libraries',
   logo: '/logo.png',
-  menu: [{ to: '/#gl-magic-libraries', text: 'magic-libraries' }],
+  menu: [
+    {
+      to: '/#gl-magic-libraries',
+      text: 'magic-libraries',
+      items: [
+        { to: '#gl-magic-libraries-li-db', text: 'db' },
+        { to: '#gl-magic-libraries-li-gql', text: 'gql' },
+        { to: '#gl-magic-libraries-li-is', text: 'is' },
+        { to: '#gl-magic-libraries-li-json', text: 'json' },
+        { to: '#gl-magic-libraries-li-uri', text: 'uri' },
+      ],
+    },
+  ],
 }
